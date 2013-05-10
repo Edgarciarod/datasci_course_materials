47e3678012249f0797f62ae692e301e91b01e28c
Edgar García Rodríguez <edgarciarod@gmail.com>
2013-05-10 12:21:58 -0500

    Ya funcionan las primeras 2 tareas

--- assignment1/WarmUp.py
+++ assignment1/WarmUp.py
@@ -8,5 +8,10 @@ import urllib2 as urllib
 
 
 for i in xrange(100):
-    response = urllib.urlopen("http://search.twitter.com/search.json?q=linux&page={}".format(i + 1))
-    print json.load(response)[u'results'][0][u'text']
+    response = urllib.urlopen("http://search.twitter.com/search.json?q=linux&page=" + str(i + 1))
+    results = json.load(response)[u'results']
+    if len(results) > 0:
+        for each_result in results:
+            print each_result[u'text']
+    else:
+        break
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,50 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def extractor(scores, tweet_data):
+    sentiment_score = 0
+    try:
+        tweet_words = tweet_data[u'text'].strip()
+        tweet_words = tweet_words.lower().split(' ')
+        tweet_words = [s.strip("#@!:)(") for s in tweet_words]
+        #print(tweet_words)
+        for each_word in tweet_words:
+            try:
+                sentiment_score += scores[each_word]
+                #print "---->"+each_word
+            except KeyError:
+                continue
+        print (float(sentiment_score))
+    except KeyError:
+
+        print (float(sentiment_score))
+        return
+
+
+def hw(sent_file, tweet_file):
+    scores = {}  # initialize an empty dictionary
+    for line in sent_file:
+        term, score = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+        #print scores.items()  # Print every (term, score) pair in the dictionary
+    all_tweets = tweet_file.readlines()
+    for tweet in all_tweets:
+        try:
+            tweet_data = json.loads(tweet)
+            extractor(scores, tweet_data)
+        except ValueError:
+            continue
+
 
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    hw(sent_file, tweet_file)
+
 
 if __name__ == '__main__':
     main()
