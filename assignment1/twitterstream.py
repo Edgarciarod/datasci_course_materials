9ebb113dd19f575cdaf8a3fbcef1985fca64583b
Edgar García Rodríguez <edgarciarod@gmail.com>
2013-06-16 19:35:40 -0500

    arraglandoproblemita

--- assignment1/twitterstream.py
+++ assignment1/twitterstream.py
@@ -4,22 +4,9 @@
 import oauth2 as oauth
 import urllib2 as urllib
 
-<< << << < HEAD
-# See Assginment 6 instructions or README for how to get these credentials
-<< << << < HEAD
-access_token_key = "144685422-pVNhlyxMKtOXVDp8j5XdRNWppvfFcy1zX0GREdzL"
-access_token_secret = "INrg9aI8b3jpYe0KzkTT8BbDvfWgMFHAlEZNFAYPpWM"
-== == == =
-# See Assignment 1 instructions or README for how to get these credentials
-access_token_key = "<Enter your access token key here>"
-access_token_secret = "<Enter your access token secret here>"
->> >> >> > 205e29
-cfb37953d2ad4ee76c56e1b80849a389bb
-== == == =
+
 access_token_key = ""
 access_token_secret = ""
->> >> >> > 7
-f8d5e66408a03ea3c3c0ffdee3e99d54017427a
 
 consumer_key = ""
 consumer_secret = ""
